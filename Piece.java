e6f5d422a86d7e461924ea0da3a366c0ab9bcdeb
cohyunoh <coh00@stuy.edu>
2019-01-04 11:47:43 -0500

    Making a few chenages to Piece as we develop better UML diagram

--- Piece.java
+++ Piece.java
@@ -1,12 +1,15 @@
 public class Piece{
+  private String name;
+  private Location square;
+  public Piece(String pieceName, Location coordinate){
+    name = pieceName;
+    square = coordinate;
+  }
 
-public Piece();
-private String name;
-private Location square;
 
-public boolean movable (String name, Location square);
-public Location move (String name, Location square);
-public boolean incheck (Location square);
-public boolean checkmated (Location square);
+  public boolean movable (String name, Location square)
+  public Location move (String name, Location square);
+  public boolean incheck (Location square);
+  public boolean checkmated (Location square);
 
 }
