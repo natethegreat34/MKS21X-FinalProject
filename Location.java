db620c3a0adc1ab1e76a19712f24303fed05e4b5
cohyunoh <coh00@stuy.edu>
2019-01-04 11:21:21 -0500

    Location.java Found the name not necessary

--- Location.java
+++ Location.java
@@ -1,11 +1,9 @@
 public class Location{
   private String place;
-  private String name;
   public Location(String coordinate, String piece){
     place = coordinate;
-    name = piece;
   }
-  public String getLocation(String name){
-    
+  public String getCoordinate(){
+    return place;
   }
 }
