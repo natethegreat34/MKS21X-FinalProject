e2c748962e7b84efafd2489fa9e877f25571068e
cohyunoh <coh00@stuy.edu>
2019-01-03 21:00:49 -0500

    Added bmove, nmove, rmove, kmove, qmove

--- Move.java
+++ Move.java
@@ -1,8 +1,28 @@
-public class Move{
+public class Move extends Location{
   private Location place;
   private String name;
 
   public Location pmove(Location coordinate){
+    return coordinate;
+  }
+
+  public Location bmove(Location coordinate){
+    return coordinate;
+  }
+
+  public Location nmove(Location coordinate){
+    return coordinate;
+  }
+
+  public Location rmove(Location coordinate){
+    return coordinate;
+  }
+
+  public Location kmove(Location coordinate){
+    return coordinate;
+  }
+
+  public Location qmove(Location coordinate){
     return coordinate;
   }
 }
