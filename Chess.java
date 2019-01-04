6900cab9562d23b6e485571bab5e42a61d10ddb4
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-01-04 11:46:29 -0500

    Experimenting with format of board,needed a visual

--- Chess.java
+++ Chess.java
@@ -1,11 +1,36 @@
 public class Chess{
+private String [] [] board;
+public Chess (){
+      board = new String [8][8];
+      clear();
+        }
+public String toString(){
+          String life = "   a b c  d  e  f  g  h";
+          life = life + "\n";
+          String sad = life;
+          int number = 8;
+          for (int x = 0; x < board.length; x ++){
+            sad = sad + + number + " |";
+            for (int y = 0; y < board[x].length; y ++){
+              sad = sad + board [x][y] + " ";
+            }
+            sad = sad + "| " + number;
+            sad = sad + "\n";
+            number --;
+        }
+        sad = sad + life;
+        return sad;
+      }
 
-public Chess();
-private GameMode type;
-
-private void clear();
-private void setup(GameMode type);
-private void save();
-
-
+private void clear(){
+for (int x = 0; x < 8; x ++){
+        for (int y = 0; y < 8; y ++){
+          board[x][y] = "__";
+        }
+      }
+    }
 }
+// private void save();
+//
+//
+// }
---
+++ ChessDriver.java
@@ -0,0 +1,6 @@
+public class ChessDriver{
+public static void main(String[] args) {
+    Chess hi = new Chess();
+    System.out.println(hi.toString());
+}
+}
