f714f3638c273d480904166d43f8075a8e2ae5a7
cohyunoh <coh00@stuy.edu>
2019-01-05 17:27:33 -0500

    Made a constructor and the four set and get methods for Pawn.java Also had to change the set methods in Piece.java

--- Pawn.java
+++ Pawn.java
@@ -1,3 +1,24 @@
 public class Pawn implements Piece{
-  
+  private int xCor;
+  private int yCor;
+  public Pawn(){
+    xCor = 0;
+    yCor = 0;
+  }
+  public int getX(){
+    return xCor;
+  }
+  public int getY(){
+    return yCor;
+  }
+  public int setX(int x){
+    int oldVal = xCor;
+    xCor = x;
+    return oldVal;
+  }
+  public int setY(int y){
+    int oldVal = yCor;
+    yCor = y;
+    return oldVal;
+  }
 }
--- Piece.java
+++ Piece.java
@@ -2,7 +2,7 @@ public interface Piece{
   public boolean move(int x, int y);
   public int getX();
   public int getY();
-  public int setX();
-  public int setY();
+  public int setX(int x);
+  public int setY(int y);
   public String getLocation();
 }
