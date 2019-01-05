3bb84ad27f36d16e0ecb28b96cc2580155ac0b28
cohyunoh <coh00@stuy.edu>
2019-01-05 16:56:41 -0500

    Made Piece an interface

--- Piece.java
+++ Piece.java
@@ -1,12 +1,3 @@
-public class Piece{
-
-public Piece();
-private String name;
-private Location square;
-
-public boolean movable (String name, Location square);
-public Location move (String name, Location square);
-public boolean incheck (Location square);
-public boolean checkmated (Location square);
-
+public interface Piece{
+  public boolean move(int x, int y);
 }
