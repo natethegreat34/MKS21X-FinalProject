2d1312e35e67eb10f5afdddf47aa42d02b03df04
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-01-05 22:44:54 -0500

    Made Piece a class and got the setup

--- Chess.java
+++ Chess.java
@@ -3,6 +3,7 @@ private String [] [] board;
 public Chess (){
       board = new String [8][8];
       clear();
+      setup();
         }
 public String toString(){
           String life = "   a b c  d  e  f  g  h";
@@ -10,7 +11,7 @@ public String toString(){
           String sad = life;
           int number = 8;
           for (int x = 0; x < board.length; x ++){
-            sad = sad + + number + " |";
+            sad = sad + number + " |";
             for (int y = 0; y < board[x].length; y ++){
               sad = sad + board [x][y] + " ";
             }
@@ -25,10 +26,47 @@ public String toString(){
 private void clear(){
 for (int x = 0; x < 8; x ++){
         for (int y = 0; y < 8; y ++){
-          board[x][y] = "__";
+          board[x][y] = "_";
         }
       }
     }
+
+    // need to call each piece and set location
+public void setup(){
+    //black rooks
+    board[0][0] = "r";
+    board[0][7] = "r";
+    //white rooks
+    board[7][0] = "R";
+    board[7][7] = "R";
+    //black knights
+    board[0][1] = "n";
+    board[0][6] = "n";
+    //white knights
+    board[7][1] = "N";
+    board[7][6] = "N";
+    //black bishops
+    board[0][2] = "b";
+    board[0][5] = "b";
+    //white bishops
+    board[7][2] = "B";
+    board[7][5] = "B";
+    //black king and queen
+    board[0][3] = "k";
+    board[0][4] = "q";
+    //white king and quen
+    board[7][3] = "K";
+    board[7][4] = "Q";
+    //black pawns
+    for (int n = 0; n < 8; n ++){
+        board [1][n] = "p";
+    }
+    //white pawns
+    for (int n = 0; n < 8; n ++){
+        board [6][n] = "P";
+    }
+
+}
 }
 // private void save();
 //
--- Piece.java
+++ Piece.java
@@ -1,8 +1,40 @@
-public interface Piece{
-  public boolean move(int x, int y);
-  public int getX();
-  public int getY();
-  public int setX();
-  public int setY();
-  public String getLocation();
+public class Piece extends Chess{
+  private int xCor;
+  private int yCor;
+  private int moveNumber;
+  public int getX(){
+    return xCor;
+  }
+  public int getY(){
+    return yCor;
+  }
+  public int setX(int x){
+    int oldVal = xCor;
+    xCor = x;
+    return oldVal;
+  }
+  public int setY(int y){
+    int oldVal = yCor;
+    yCor = y;
+    return oldVal;
+  }
+  public boolean move(String piece, int x, int y){
+      //white pieces
+    if (piece.equals("K")){
+
+    }
+    if (piece.equals("Q"))
+    if (piece.equals("R"))
+    if (piece.equals("B"))
+    if (piece.equals("N"))
+    if (piece.equals("P"))
+    return false;
+  }
+  public String getLocation(){
+    String ans = "";
+    String letters = "abcdefgh";
+    ans += letters.substring(xCor,xCor+1);
+    ans += yCor;
+    return ans;
+  }
 }
