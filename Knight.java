078464a757d22ea19ee1271e30852b8cebae7015
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-01-06 23:08:18 -0500

    Just made a layout for each piece's class

---
+++ Knight.java
@@ -0,0 +1,31 @@
+public class Knight extends Chess{
+  private int xCor;
+  private int yCor;
+  private int moveNumber;
+  public int getX(){
+    return xCor;
+  }
+  public int getY(){
+    return yCor;
+  }
+  public int setX(int x){
+    int oldVal = xCor;
+    xCor = x;
+    return oldVal;
+  }
+  public int setY(int y){
+    int oldVal = yCor;
+    yCor = y;
+    return oldVal;
+  }
+  public boolean move(int x, int y){
+        ;
+  }
+  public String getLocation(){
+    String ans = "";
+    String letters = "abcdefgh";
+    ans += letters.substring(xCor,xCor+1);
+    ans += yCor;
+    return ans;
+  }
+}
