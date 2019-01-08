07b01aaf2d7bfbfb186e6817c6a5702b0bbfd499
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-01-08 11:40:01 -0500

    Did the square class

---
+++ Square.java
@@ -0,0 +1,29 @@
+public class Square {
+
+private Piece stored;
+private boolean taken;
+private int xCor;
+private int yCor;
+
+public Square (){
+    stored = null;
+    taken = false;
+}
+public boolean isEmpty (){
+    return !taken;
+}
+
+public Piece getPiece(){
+    return stored;
+}
+
+public void setPiece(Piece noob){
+    stored = noob;
+}
+
+public Piece removePiece(){
+    Piece bye = stored;
+    stored = null;
+    return bye;
+}
+}
