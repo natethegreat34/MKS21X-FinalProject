b8e5d261d25de7cb12028f39cd985b643ad336e3
cohyunoh <coh00@stuy.edu>
2019-01-08 16:03:08 -0500

    Started coding canMove method. Realized what the actual functionality of it should be

--- Chessboard.java
+++ Chessboard.java
@@ -1,8 +1,18 @@
 public class Chessboard{
   private Square[][] data;
+  public boolean canMove(int x1, int y1, int[][] possibleMoves){
+    for (int x = 0; x < possibleMoves.length; x++){
+      if (x == x1){
+        if (possiblemoves[x][1] == y1){
+          if (data[x1][y1].isEmpty() || 
+        }
+      }
+    }
+  }
   public boolean move(int x1, int y1, int x2, int y2){
     Piece movePiece = data[x1][y1].getPiece();
     int[][] possibleMoves = movePiece.getMoveList();
-    for (int x = 0; x < possibleMove.length
+    for (int x = 0; x < possibleMove.length; x++){
+	
   }
 }
