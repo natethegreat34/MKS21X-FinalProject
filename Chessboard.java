b0d459ed3eec5f9892e3ea6220ab1dba5c0f098c
cohyunoh <coh00@stuy.edu>
2019-01-09 23:13:09 -0500

    Made a basic constructor for chessboard when there is a given filename

--- Chessboard.java
+++ Chessboard.java
@@ -1,6 +1,104 @@
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class Chessboard{
   private Square[][] data;
+  public Chessboard(String filename) throws FileNotFoundException{
+    data = Square[8][8];
+    File f = new File(filename);
+    Scanner in = new Scanner(f);
+    for (int x = 0; x < data.length; x++){
+      for (int y = 0; y < data[x].length; y++){
+        //read the next letter from the file
+        String piece = in.next();
+        //if it is a black Pawn
+        if (piece.equals("bp")){
+          Square slot = new Square();
+          Piece pawn = new Pawn(x, y, "black");
+          slot.setPiece(pawn);
+          data[x][y] = slot;
+        }
+        //if it is a black Bishop
+        if (piece.equals("bb")){
+          Square slot = new Square();
+          Piece bishop = new Bishop(x, y, "black");
+          slot.setPiece(bishop);
+          data[x][y] = slot;
+        }
+        //if it is a black Knight
+        if (piece.equals("bn")){
+          Square slot = new Square();
+          Piece knight = new Knight(x, y, "black");
+          slot.setPiece(knight);
+          data[x][y] = slot;
+        }
+        //if it is a black Rook
+        if (piece.equals("br")){
+          Square slot = new Square();
+          Piece rook = new Rook(x, y, "black");
+          slot.setPiece(rook);
+          data[x][y] = slot;
+        }
+        //if it is a black Queen
+        if (piece.equals("bq")){
+          Square slot = new Square();
+          Piece queen = new Queen(x, y, "black");
+          slot.setPiece(queen);
+          data[x][y] = slot;
+        }
+        //if it is a black King
+        if (piece.equals("bk")){
+          Square slot = new Square();
+          Piece king = new King(x, y, "black");
+          slot.setPiece(king);
+          data[x][y] = slot;
+        }
+        //if it is a white pawn
+        if (piece.equals("wp")){
+          Square slot = new Square();
+          Piece pawn = new Pawn(x, y, "white");
+          slot.setPiece(pawn);
+          data[x][y] = slot;
+        }
+        //if it is a white Bishop
+        if (piece.equals("wb")){
+          Square slot = new Square();
+          Piece bishop = new Bishop(x, y, "white");
+          slot.setPiece(bishop);
+          data[x][y] = slot;
+        }
+        //if it is a white Knight
+        if (piece.equals("wn")){
+          Square slot = new Square();
+          Piece knight = new Knight(x, y, "white");
+          slot.setPiece(knight);
+          data[x][y] = slot;
+        }
+        //if it is a white Rook
+        if (piece.equals("wr")){
+          Square slot = new Square();
+          Piece rook = new Rook(x, y, "white");
+          slot.setPiece(rook);
+          data[x][y] = slot;
+        }
+        //if it is a white Queen
+        if (piece.equals("wq")){
+          Square slot = new Square();
+          Piece queen = new Queen(x, y, "white");
+          slot.setPiece(queen);
+          data[x][y] = slot;
+        }
+        //if it is a white King
+        if (piece.equals("wk")){
+          Square slot = new Square();
+          Piece king = new King(x, y, "white");
+          slot.setPiece(king);
+          data[x][y] = slot;
+        }
+      }
+    }
+  }
   public String[][] possibleMoves(int x, int y, String[][] moveList){
     String[][] ans = new String[8][8];
     int greatestX = 0;
