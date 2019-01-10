6309a9da639f4526b84a7f587d9c1a888a90bf4a
cohyunoh <coh00@stuy.edu>
2019-01-10 11:44:16 -0500

    readded pawn methods and constructir

--- Pawn.java
+++ Pawn.java
@@ -1,4 +1,22 @@
 public class Pawn {
+  private int xCor;
+  private int yCor;
+  private String color;
+  private String type;
+  public Pawn(int x, int y, String colorWB){
+    color = colorWB;
+    if (color.equals("black")){
+      type = "bp";
+    }
+    if (color.equals("white")){
+      type = "wp";
+    }
+    xCor = x;
+    yCor = y;
+  }
+  public String getType(){
+    return type;
+  }
     public String [][] possiblemoves(int x, int y){
         String [][] ppm = new String [8][8];
         if (y + 2 < 8){
