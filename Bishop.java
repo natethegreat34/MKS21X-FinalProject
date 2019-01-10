d39da4824a188b489c7221d627a1a30baa36a7aa
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-01-10 11:24:07 -0500

    Fixed

--- Bishop.java
+++ Bishop.java
@@ -1,22 +1,4 @@
 public class Bishop{
-    private int xCor;
-    private int yCor;
-    private String color;
-    private String type;
-    public Bishop(int x, int y, String colorWB){
-      color = colorWB;
-      if (color.equals("black")){
-        type = "bb";
-      }
-      if (color.equals("white")){
-        type = "wb";
-      }
-      xCor = x;
-      yCor = y;
-    }
-    public String getType(){
-      return type;
-    }
     //Bishop
     //for going / x - y = z; (x+-i)-(y+-i) = z
     // for going \ x + y = z; (x+-i)+(y+-i) = z
@@ -38,6 +20,7 @@ public class Bishop{
         for (int i = 0; x - i < 8 && y - i < 8; i ++){
             bpm [x - i] [y - i] = "o";
         }
+        bpm [x] [y] = "*";
         return bpm;
 
         }
