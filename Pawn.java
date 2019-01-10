813c7bb395dfd7c2cd191e0238953ae0fb28f02e
cohyunoh <coh00@stuy.edu>
2019-01-09 23:28:25 -0500

    Pawn.java is et up for testing if it will be displayed on the board

--- Pawn.java
+++ Pawn.java
@@ -1,31 +1,18 @@
-public class Pawn extends Chess{
+public class Pawn extends Piece{
   private int xCor;
   private int yCor;
-  private int moveNumber;
-  public int getX(){
-    return xCor;
+  private String color;
+  private String type;
+  public Pawn(String colorWB){
+    color = colorWB;
+    if (color.equals("black")){
+      type = "bp";
+    }
+    if (color.equals("white")){
+      type = "wp";
+    }
   }
-  public int getY(){
-    return yCor;
-  }
-  public int setX(int x){
-    int oldVal = xCor;
-    xCor = x;
-    return oldVal;
-  }
-  public int setY(int y){
-    int oldVal = yCor;
-    yCor = y;
-    return oldVal;
-  }
-  public boolean move(int x, int y){
-        ;
-  }
-  public String getLocation(){
-    String ans = "";
-    String letters = "abcdefgh";
-    ans += letters.substring(xCor,xCor+1);
-    ans += yCor;
-    return ans;
+  public String getType(){
+    return type;
   }
 }
