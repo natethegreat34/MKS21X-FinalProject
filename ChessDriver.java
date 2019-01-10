6ab17b33dc2da9925a15046c3ff8fbe62e7d0e34
cohyunoh <coh00@stuy.edu>
2019-01-09 23:43:00 -0500

    ChessDriver is ready for testing

--- ChessDriver.java
+++ ChessDriver.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class ChessDriver{
-  public static void main(String[] args) {
+  public static void main(String[] args) throws FileNotFoundException{
     Chessboard chess = new Chessboard("file1.txt");
     System.out.println(chess);
   }
