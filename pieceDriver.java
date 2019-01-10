97c3430659b0e7f5ee7fbcc260d256342d42079e
cohyunoh <coh00@stuy.edu>
2019-01-10 11:39:38 -0500

    oopsies switched 7 and 2

--- pieceDriver.java
+++ pieceDriver.java
@@ -1,6 +1,6 @@
 public class pieceDriver{
     public static void main(String[] args) {
-      Bishop bishop = new Bishop(2, 7, "black");
+      Bishop bishop = new Bishop(7, 2, "black");
       System.out.println(Piece.movesString(bishop.possiblemoves()));
     }
 }
