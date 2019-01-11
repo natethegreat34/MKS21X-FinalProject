c80e85ad16f4c8b61d7c7c15988a776a5be5bab1
cohyunoh <coh00@stuy.edu>
2019-01-10 21:57:46 -0500

    Added a boolean to pawn to check if the pawn can move 2 places

--- Pawn.java
+++ Pawn.java
@@ -5,7 +5,7 @@ public class Pawn extends Piece {
   private String type;
   private String[][] data;
   private int direction;
-
+  private boolean move2;
   public Pawn(int x, int y, String colorWB, int dir){
     color = colorWB;
     if (color.equals("black")){
@@ -17,6 +17,7 @@ public class Pawn extends Piece {
     xCor = x;
     yCor = y;
     direction = dir%2;
+    move2 = true;
     possibleMoves();
   }
 
@@ -32,25 +33,37 @@ public class Pawn extends Piece {
     data = inpt;
   }
 
+  public boolean canMove2(){
+    return move2;
+  }
+
+  public void setMove2(boolean inpt){
+    move2 = inpt;
+  }
+
   private void possibleMoves(){
       String [][] ppm = new String [8][8];
       if(direction == 0){
+        if(move2){
           if (yCor + 2 < 8){
-              // ^^ first time moving import junit.framework.TestCase;
-              ppm [yCor + 2] [xCor] = "o";
-            }
-              // ^
-          if (yCor + 1 < 8){
-              // ^^ anytime moving import junit.framework.TestCase;
-            ppm [yCor + 1] [xCor] = "o";
-            }
-          }
-      if (direction == 1){
-        if (yCor - 2 < 8){
             // ^^ first time moving import junit.framework.TestCase;
-            ppm [yCor - 2] [xCor] = "o";
+            ppm [yCor + 2] [xCor] = "o";
+          }
         }
-            // ^
+        // ^
+        if (yCor + 1 < 8){
+            // ^^ anytime moving import junit.framework.TestCase;
+          ppm [yCor + 1] [xCor] = "o";
+          }
+        }
+      if (direction == 1){
+        if(move2){
+          if (yCor - 2 < 8){
+              // ^^ first time moving import junit.framework.TestCase;
+              ppm [yCor - 2] [xCor] = "o";
+          }
+        }
+        // ^
         if (yCor - 1 < 8){
             // ^^ anytime moving import junit.framework.TestCase;
             ppm [yCor - 1] [xCor] = "o";
