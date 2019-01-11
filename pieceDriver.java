e987411b930f2e1377d0aec8d72734535cf6c55c
cohyunoh <coh00@stuy.edu>
2019-01-10 20:20:10 -0500

    testing rooks' possiblemoves method

--- pieceDriver.java
+++ pieceDriver.java
@@ -2,5 +2,7 @@ public class pieceDriver{
     public static void main(String[] args) {
       Bishop bishop = new Bishop(7, 2, "black");
       System.out.println(Piece.movesString(bishop.possiblemoves()));
+      Rook rook = new Rook(7, 1, "white");
+      System.out.println(Piece.movesString(rook.possiblemoves()));
     }
 }
