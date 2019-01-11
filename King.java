631436cb664ed74cf8df890e85b7f9f5be20c355
cohyunoh <coh00@stuy.edu>
2019-01-10 20:38:54 -0500

    trying to configure king so that its possible moves method works

--- King.java
+++ King.java
@@ -26,21 +26,21 @@ public class King extends Piece{
 
   public String [][] possiblemoves(){
       String [][] kpm = new String [8][8];
-      if (yCor+ 1 < 8){
+      if (yCor + 1 < 7){
         // ^
         kpm [xCor] [yCor+ 1] = "o";
       }
-      if (yCor- 1 >= 0){
+      if (yCor - 1 >= 0){
         // v
         kpm [xCor] [yCor- 1] = "o";
       }
-      if (xCor+ 1 < 8 && yCor+ 1 < 8){
+      if (xCor+ 1 < 7 && yCor+ 1 < 7){
         // />
         kpm [xCor+ 1] [yCor+ 1] = "o";
         // >
         kpm [xCor+ 1] [yCor] = "o";
       }
-      if (xCor+ 1 < 8 && yCor- 1 >= 0){
+      if (xCor+ 1 < 7 && yCor- 1 >= 0){
         // \>
         kpm [xCor+ 1] [yCor- 1] = "o";
       }
@@ -50,11 +50,11 @@ public class King extends Piece{
         // <
         kpm [xCor- 1] [yCor] = "o";
       }
-      if (xCor- 1 >= 0 && yCor+ 1 < 8){
+      if (xCor- 1 >= 0 && yCor+ 1 < 7){
         // <\
         kpm [xCor- 1] [yCor+ 1] = "o";
       }
-      if (xCor+ 2 < 8 || xCor- 2 >= 0){
+      if (xCor+ 2 < 6 || xCor- 2 >= 0){
         //Castle Kingside
         kpm [xCor+ 2] [yCor] = "o";
         //Castle Queenside
--- pieceDriver.java
+++ pieceDriver.java
@@ -2,7 +2,12 @@ public class pieceDriver{
     public static void main(String[] args) {
       Bishop bishop = new Bishop(7, 2, "black");
       System.out.println(Piece.movesString(bishop.possiblemoves()));
+      System.out.println("bb".equals(bishop.getType()));
       Rook rook = new Rook(7, 1, "white");
       System.out.println(Piece.movesString(rook.possiblemoves()));
+      System.out.println("wr".equals(rook.getType()));
+      King king = new King(7, 3, "white");
+      System.out.println(Piece.movesString(king.possiblemoves()));
+      System.out.println("wk".equals(king.getType()));
     }
 }
