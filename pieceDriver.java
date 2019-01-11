fdf955fadf659cba1da7d6cf2d86b2fd1c6c6975
cohyunoh <coh00@stuy.edu>
2019-01-10 22:02:50 -0500

    Updated driver to match with all the changes to the pieces

--- pieceDriver.java
+++ pieceDriver.java
@@ -1,16 +1,16 @@
 public class pieceDriver{
     public static void main(String[] args) {
       Bishop bishop = new Bishop(2, 7, "black");
-      System.out.println(Piece.movesString(bishop.possiblemoves()));
+      System.out.println(Piece.movesString(bishop.getData()));
       System.out.println("bb".equals(bishop.getType()));
       Rook rook = new Rook(1, 7, "white");
-      System.out.println(Piece.movesString(rook.possiblemoves()));
+      System.out.println(Piece.movesString(rook.getData()));
       System.out.println("wr".equals(rook.getType()));
       King king = new King(3, 7, "white");
-      System.out.println(Piece.movesString(king.possiblemoves()));
+      System.out.println(Piece.movesString(king.getData()));
       System.out.println("wk".equals(king.getType()));
-      Pawn pawn = new Pawn(3, 6, "white");
-      System.out.println(Piece.movesString(pawn.possiblemoves()));
+      Pawn pawn = new Pawn(3, 6, "white", 0);
+      System.out.println(Piece.movesString(pawn.getData()));
       System.out.println("wp".equals(pawn.getType()));
     }
 }
