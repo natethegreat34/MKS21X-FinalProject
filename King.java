2ffe4da279edbd4f3187485628caee6401b257a4
cohyunoh <coh00@stuy.edu>
2019-01-10 20:25:43 -0500

    doing some formatting for the methods in all classes

--- King.java
+++ King.java
@@ -18,42 +18,42 @@ public class King extends Piece{
     return type;
   }
 
-    public String [][] possiblemoves(int x, int y){
-        String [][] kpm = new String [8][8];
-        if (y + 1 < 8){
+  public String [][] possiblemoves(int x, int y){
+      String [][] kpm = new String [8][8];
+      if (y + 1 < 8){
         // ^
         kpm [x] [y + 1] = "o";
-    }
-        if (y - 1 >= 0){
+      }
+      if (y - 1 >= 0){
         // v
         kpm [x] [y - 1] = "o";
-    }
-        if (x + 1 < 8 && y + 1 < 8){
+      }
+      if (x + 1 < 8 && y + 1 < 8){
         // />
         kpm [x + 1] [y + 1] = "o";
         // >
         kpm [x + 1] [y] = "o";
-    }
-        if (x + 1 < 8 && y - 1 >= 0){
+      }
+      if (x + 1 < 8 && y - 1 >= 0){
         // \>
         kpm [x + 1] [y - 1] = "o";
-    }
-        if (x - 1 >=0 && y - 1 >= 0){
+      }
+      if (x - 1 >=0 && y - 1 >= 0){
         // </
         kpm [x - 1] [y - 1] = "o";
         // <
         kpm [x - 1] [y] = "o";
-    }
-        if (x - 1 >= 0 && y + 1 < 8){
+      }
+      if (x - 1 >= 0 && y + 1 < 8){
         // <\
         kpm [x - 1] [y + 1] = "o";
-    }
-    if (x + 2 < 8 || x - 2 >= 0){
+      }
+      if (x + 2 < 8 || x - 2 >= 0){
         //Castle Kingside
         kpm [x + 2] [y] = "o";
         //Castle Queenside
         kpm [x - 2] [y] = "o";
-    }
-        return kpm;
       }
+      return kpm;
+    }
 }
