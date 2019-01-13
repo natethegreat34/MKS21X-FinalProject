366265ae480505b6d450fcb287b01af3bd0485e3
cohyunoh <coh00@stuy.edu>
2019-01-12 23:57:31 -0500

    Made it so all the fill methods clear the board before filling

--- Chessboard.java
+++ Chessboard.java
@@ -18,9 +18,23 @@ public class Chessboard{
 
 
   //==============================================================================
+  public void clear(){
+    for (int y = 0; y < data.length; y++){
+      for (int x = 0; x < data[y].length; x++){
+        Square empty = new Square (x,y);
+        data[y][x] = empty;
+      }
+    }
+  }
+  //------------------------------------------------------------------------------
+
+
+
+  //==============================================================================
   public void fillBoard(String filename) throws FileNotFoundException{
     File f = new File(filename);
     Scanner in = new Scanner(f);
+    clear();
     for (int y = 0; y < data.length; y++){
       for (int x = 0; x < data[y].length; x++){
         //read the next letter from the file
@@ -513,63 +527,64 @@ public class Chessboard{
 
   //==============================================================================
   public void fill (){
-        Pawn piece1 = new Pawn (0,1, "black", 0); Rook piece17 = new Rook (0,0, "black");
-        data[1][0].setPiece(piece1); data[0][0].setPiece(piece17);
-        Pawn piece2 = new Pawn (1,1, "black", 0); Knight piece18 = new Knight (1, 0, "black");
-        data[1][1].setPiece(piece2); data[0][1].setPiece(piece18);
-        Pawn piece3 = new Pawn (2,1, "black", 0); Bishop piece19 = new Bishop (2, 0, "black");
-        data[1][2].setPiece(piece3); data[0][2].setPiece(piece19);
-        Pawn piece4 = new Pawn (3,1, "black", 0); Queen piece20 = new Queen (3, 0, "black");
-        data[1][3].setPiece(piece4); data[0][3].setPiece(piece20);
-        Pawn piece5 = new Pawn (4,1, "black", 0); King piece21 = new King (4, 0, "black");
-        data[1][4].setPiece(piece5); data[0][4].setPiece(piece21);
-        Pawn piece6 = new Pawn (5,1, "black", 0); Bishop piece22 = new Bishop (5, 0, "black");
-        data[1][5].setPiece(piece6); data[0][5].setPiece(piece22);
-        Pawn piece7 = new Pawn (6,1, "black", 0); Knight piece23 = new Knight (6, 0, "black");
-        data[1][6].setPiece(piece7); data[0][6].setPiece(piece23);
-        Pawn piece8 = new Pawn (7,1, "black", 0); Rook piece24 = new Rook (7, 0, "black");
-        data[1][7].setPiece(piece8); data[0][7].setPiece(piece24);
+    clear();
+    Pawn piece1 = new Pawn (0,1, "black", 0); Rook piece17 = new Rook (0,0, "black");
+    data[1][0].setPiece(piece1); data[0][0].setPiece(piece17);
+    Pawn piece2 = new Pawn (1,1, "black", 0); Knight piece18 = new Knight (1, 0, "black");
+    data[1][1].setPiece(piece2); data[0][1].setPiece(piece18);
+    Pawn piece3 = new Pawn (2,1, "black", 0); Bishop piece19 = new Bishop (2, 0, "black");
+    data[1][2].setPiece(piece3); data[0][2].setPiece(piece19);
+    Pawn piece4 = new Pawn (3,1, "black", 0); Queen piece20 = new Queen (3, 0, "black");
+    data[1][3].setPiece(piece4); data[0][3].setPiece(piece20);
+    Pawn piece5 = new Pawn (4,1, "black", 0); King piece21 = new King (4, 0, "black");
+    data[1][4].setPiece(piece5); data[0][4].setPiece(piece21);
+    Pawn piece6 = new Pawn (5,1, "black", 0); Bishop piece22 = new Bishop (5, 0, "black");
+    data[1][5].setPiece(piece6); data[0][5].setPiece(piece22);
+    Pawn piece7 = new Pawn (6,1, "black", 0); Knight piece23 = new Knight (6, 0, "black");
+    data[1][6].setPiece(piece7); data[0][6].setPiece(piece23);
+    Pawn piece8 = new Pawn (7,1, "black", 0); Rook piece24 = new Rook (7, 0, "black");
+    data[1][7].setPiece(piece8); data[0][7].setPiece(piece24);
 
-        Pawn piece9 = new Pawn (0,6, "white", 1); Rook piece25 = new Rook (0,7, "white");
-        data[6][0].setPiece(piece9); data[7][0].setPiece(piece25);
-        Pawn piece10 = new Pawn (1,6, "white", 1);Knight piece26 = new Knight (1, 7, "white");
-        data[6][1].setPiece(piece10); data[7][1].setPiece(piece26);
-        Pawn piece11 = new Pawn (2,6, "white", 1);Bishop piece27 = new Bishop (2, 7, "white");
-        data[6][2].setPiece(piece11); data[7][2].setPiece(piece27);
-        Pawn piece12 = new Pawn (3,6, "white", 1);Queen piece28 = new Queen (3, 7, "white");
-        data[6][3].setPiece(piece12); data[7][3].setPiece(piece28);
-        Pawn piece13 = new Pawn (4,6, "white", 1);King piece29 = new King (4, 7, "white");
-        data[6][4].setPiece(piece13); data[7][4].setPiece(piece29);
-        Pawn piece14 = new Pawn (5,6, "white", 1);Bishop piece30 = new Bishop (5, 7, "white");
-        data[6][5].setPiece(piece14); data[7][5].setPiece(piece30);
-        Pawn piece15 = new Pawn (6,6, "white", 1);Knight piece31 = new Knight (6, 7, "white");
-        data[6][6].setPiece(piece15); data[7][6].setPiece(piece31);
-        Pawn piece16 = new Pawn (7,6, "white", 1);Rook piece32 = new Rook (7, 7, "white");
-        data[6][7].setPiece(piece16); data[7][7].setPiece(piece32);
+    Pawn piece9 = new Pawn (0,6, "white", 1); Rook piece25 = new Rook (0,7, "white");
+    data[6][0].setPiece(piece9); data[7][0].setPiece(piece25);
+    Pawn piece10 = new Pawn (1,6, "white", 1);Knight piece26 = new Knight (1, 7, "white");
+    data[6][1].setPiece(piece10); data[7][1].setPiece(piece26);
+    Pawn piece11 = new Pawn (2,6, "white", 1);Bishop piece27 = new Bishop (2, 7, "white");
+    data[6][2].setPiece(piece11); data[7][2].setPiece(piece27);
+    Pawn piece12 = new Pawn (3,6, "white", 1);Queen piece28 = new Queen (3, 7, "white");
+    data[6][3].setPiece(piece12); data[7][3].setPiece(piece28);
+    Pawn piece13 = new Pawn (4,6, "white", 1);King piece29 = new King (4, 7, "white");
+    data[6][4].setPiece(piece13); data[7][4].setPiece(piece29);
+    Pawn piece14 = new Pawn (5,6, "white", 1);Bishop piece30 = new Bishop (5, 7, "white");
+    data[6][5].setPiece(piece14); data[7][5].setPiece(piece30);
+    Pawn piece15 = new Pawn (6,6, "white", 1);Knight piece31 = new Knight (6, 7, "white");
+    data[6][6].setPiece(piece15); data[7][6].setPiece(piece31);
+    Pawn piece16 = new Pawn (7,6, "white", 1);Rook piece32 = new Rook (7, 7, "white");
+    data[6][7].setPiece(piece16); data[7][7].setPiece(piece32);
 
-    }
-    //------------------------------------------------------------------------------
+  }
+  //------------------------------------------------------------------------------
 
 
 
-    //==============================================================================
-    public String toString(){
-      String ans = "";
-      for (int y = 0; y < data.length; y++){
-        String line = "";
-        for (int x = 0; x < data[y].length; x++){
-          if(data[y][x].isEmpty()){
-            line += "_ ";
-          }
-          else{
-            line += data[y][x].getPiece().getType() + " ";
-          }
+  //==============================================================================
+  public String toString(){
+    String ans = "";
+    for (int y = 0; y < data.length; y++){
+      String line = "";
+      for (int x = 0; x < data[y].length; x++){
+        if(data[y][x].isEmpty()){
+          line += "_ ";
         }
-        line += "\n";
-        ans += line;
+        else{
+          line += data[y][x].getPiece().getType() + " ";
+        }
       }
-      return ans;
+      line += "\n";
+      ans += line;
     }
+    return ans;
+  }
 
 //for the possibleMoves method, it will use the loops from the classes and just perform them all and then check the first instances of
 //blocking pieces and pieces to be taken
