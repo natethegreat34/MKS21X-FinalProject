de46fff8b755740184c808ee1f899b2847193133
cohyunoh <coh00@stuy.edu>
2019-01-20 12:17:45 -0500

    Trying to bring it back to the old way as it was in

--- ChessGame/Knight.java
+++ ChessGame/Knight.java
@@ -19,9 +19,68 @@ public class Knight extends Piece{
     yCor = y;
     possibleMoves();
   }
-  
-
-  private void possibleMoves(){
+  //returns the xcoordinate
+   public int getX(){
+     return xCor;
+   }
+   //returns the ycoordinate
+   public int getY(){
+     return yCor;
+   }
+   //sets the xcoordinate
+   public int setX(int x){
+     int oldVal = xCor;
+     xCor = x;
+     return oldVal;
+   }
+   //sets the ycoordinate
+   public int setY(int y){
+     int oldVal = yCor;
+     yCor = y;
+     return oldVal;
+   }
+   //returns thec color
+   public String getColor(){
+     return color;
+   }
+   //returns the type
+   public String getType(){
+     return type;
+   }
+   // returns its data which shows all the possible moves
+   public String[][] getData(){
+     return data;
+   }
+   // sets the data, used in limitpiece and move
+   public void setData(String[][] inpt){
+     data = inpt;
+   }
+   //returns the location
+   public String getLocation(){
+     String ans = "";
+     String letters = "abcdefgh";
+     ans += letters.substring(xCor,xCor+1);
+     ans += yCor;
+     return ans;
+   }
+   //tells how many times the piece moved
+   public void increaseMoveNumber(){
+     moveNumber++;
+   }
+   //returns move number it is at
+   public int getMoveNumber(){
+     return moveNumber;
+   }
+   //this will reset its data if it moves its
+   public void updateData(){
+     possibleMoves();
+   }
+   //only really used with pawn to set which direction it can move
+   public void setDir(int dir){
+     direction = dir % 2;
+   }
+   //based on its coordinates it will make the possible moves
+   private void possibleMoves(){
     data = new String[8][8];
     if (yCor + 1 < 8){
       if (xCor + 2 < 8){
--- ChessGame/Pawn.java
+++ ChessGame/Pawn.java
@@ -6,7 +6,7 @@ public class Pawn extends Piece {
   private int direction;
   private int moveNumber = 0;
   private String[][] data;
-  
+
   public Pawn(int x, int y, String colorWB, int dir){
     color = colorWB;
     if (color.equals("black")){
@@ -20,8 +20,68 @@ public class Pawn extends Piece {
     direction = dir % 2;
     possibleMoves();
   }
-
-  private void possibleMoves(){
+  //returns the xcoordinate
+   public int getX(){
+     return xCor;
+   }
+   //returns the ycoordinate
+   public int getY(){
+     return yCor;
+   }
+   //sets the xcoordinate
+   public int setX(int x){
+     int oldVal = xCor;
+     xCor = x;
+     return oldVal;
+   }
+   //sets the ycoordinate
+   public int setY(int y){
+     int oldVal = yCor;
+     yCor = y;
+     return oldVal;
+   }
+   //returns thec color
+   public String getColor(){
+     return color;
+   }
+   //returns the type
+   public String getType(){
+     return type;
+   }
+   // returns its data which shows all the possible moves
+   public String[][] getData(){
+     return data;
+   }
+   // sets the data, used in limitpiece and move
+   public void setData(String[][] inpt){
+     data = inpt;
+   }
+   //returns the location
+   public String getLocation(){
+     String ans = "";
+     String letters = "abcdefgh";
+     ans += letters.substring(xCor,xCor+1);
+     ans += yCor;
+     return ans;
+   }
+   //tells how many times the piece moved
+   public void increaseMoveNumber(){
+     moveNumber++;
+   }
+   //returns move number it is at
+   public int getMoveNumber(){
+     return moveNumber;
+   }
+   //this will reset its data if it moves its
+   public void updateData(){
+     possibleMoves();
+   }
+   //only really used with pawn to set which direction it can move
+   public void setDir(int dir){
+     direction = dir % 2;
+   }
+   //based on its coordinates it will make the possible moves
+   private void possibleMoves(){
       String [][] ppm = new String [8][8];
       if(direction == 0){
         if(super.getMoveNumber() > 0){
