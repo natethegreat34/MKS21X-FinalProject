564f2d46b7ac3484d396f7beed51b3d5c4866048
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-01-20 21:27:27 -0500

    Worked on promotion

---
+++ promote.java
@@ -0,0 +1,14 @@
+public class promote{
+    public boolean move(int xCor, int yCor, int x, int y){
+        if (inpt.getColor().equals("white"){
+            if (y == 0){
+                Queen newlywed = new Queen(x, 0, "white");
+            }
+        if (inpt.getColor().equals("black"){
+            if (y == 7){
+                Queen newlywed = new Queen(x, 7, "black");
+            }
+
+        }
+    }
+}
