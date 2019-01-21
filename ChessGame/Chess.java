273c55cfe3d93b83195974a77ea9e4d4c09cbf68
cohyunoh <coh00@stuy.edu>
2019-01-20 23:58:45 -0500

    trying to get the basic display to work

--- ChessGame/Chess.java
+++ ChessGame/Chess.java
@@ -70,7 +70,7 @@ public class Chess {
 					terminal.applyBackgroundColor(Terminal.Color.WHITE);
 				}
 				char inpt = Chess.convertIntoPiece(chess[y][x]);
-				terminal.moveCursor(x + 28, y + 28);
+				terminal.moveCursor(x + 28, y);
 				terminal.putCharacter(inpt);
 				color += 1;
 				color = color%2;
