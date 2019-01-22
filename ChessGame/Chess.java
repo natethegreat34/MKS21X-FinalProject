d7d91f33cad7361074fb1c48dcc6d5cf12d99637
cohyunoh <coh00@stuy.edu>
2019-01-21 19:26:39 -0500

    Using colors now

--- ChessGame/Chess.java
+++ ChessGame/Chess.java
@@ -7,6 +7,23 @@ import java.io.FileWriter;
 import java.util.HashSet;
 import java.util.Set;
 public class Chess{
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+	public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+	public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+	public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 	//------------------------------------------------------------------------------
 
 
@@ -89,40 +106,40 @@ public class Chess{
 			return ans;
 		}
 		if(piece.equals("p")){
-			ans = '\u265F' + "";
+			ans = ANSI_WHITE_BACKGROUND + '\u265F' + "" + ANSI_RESET;
 		}
 		if(piece.equals("P")){
-			ans = '\u2659' + "";
+			ans = ANSI_WHITE_BACKGROUND + '\u2659' + "" + ANSI_RESET;
 		}
 		if(piece.equals("k")){
-			ans = '\u265A' + "";
+			ans = ANSI_WHITE_BACKGROUND + '\u265A' + "" + ANSI_RESET;
 		}
 		if(piece.equals("K")){
-			ans = '\u2654' + "";
+			ans = ANSI_WHITE_BACKGROUND + '\u2654' + "" + ANSI_RESET;
 		}
 		if(piece.equals("q")){
-			ans = '\u265B' + "";
+			ans = ANSI_WHITE_BACKGROUND + '\u265B' + "" + ANSI_RESET;
 		}
 		if(piece.equals("Q")){
-			ans = '\u2655' + "";
+			ans = ANSI_WHITE_BACKGROUND + '\u2655' + "" + ANSI_RESET;
 		}
 		if(piece.equals("r")){
-			ans = '\u265C' + "";
+			ans = ANSI_WHITE_BACKGROUND + '\u265C' + "" + ANSI_RESET;
 		}
 		if(piece.equals("R")){
-			ans = '\u2656' + "";
+			ans = ANSI_WHITE_BACKGROUND + '\u2656' + "" + ANSI_RESET;
 		}
 		if(piece.equals("b")){
-			ans = '\u265D' + "";
+			ans = ANSI_WHITE_BACKGROUND + '\u265D' + "" + ANSI_RESET;
 		}
 		if(piece.equals("B")){
-			ans = '\u2657' + "";
+			ans = ANSI_WHITE_BACKGROUND + '\u2657' + "" + ANSI_RESET;
 		}
 		if(piece.equals("n")){
-			ans = '\u265E' + "";
+			ans = ANSI_WHITE_BACKGROUND + '\u265E' + "" + ANSI_RESET;
 		}
 		if(piece.equals("N")){
-			ans = '\u2658' + "";
+			ans = ANSI_WHITE_BACKGROUND + '\u2658' + "" + ANSI_RESET;
 		}
 		return ans;
 	}
